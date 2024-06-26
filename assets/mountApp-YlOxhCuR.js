5089345a7d292ade4420714526c138d1d1295a7d
kanzululum <masbude@outlook.com>
2024-06-26 20:33:37 +0700

    fix hero

--- assets/mountApp-YlOxhCuR.js
+++
@@ -1,1 +0,0 @@
-import{S as s,j as e,T as r,U as o,V as a,W as n}from"./reachHideWarning-C1upC1Ww.js";const d=t=>{s.createRoot(document.getElementById("root")).render(e.jsx(r,{store:o,children:e.jsx(a,{loading:null,persistor:n,children:t})}))};export{d as m};
